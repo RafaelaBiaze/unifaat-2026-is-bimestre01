5ee22b40f26f46753725b640beeb3c8c47e84d7e
RafaelaBiaze <rafaelabiaze@gmail.com>
2026-03-30 16:36:13 -0300

    Add: TF de Cronjob.

---
+++ app/Commands/SyncCommand.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'sync',
+    description: 'comando de sincronização',
+    arguments: {},
+
+    handle: async function () {
+        console.log("sync executado");
+    }
+}
--- app/Http/Controllers/WebhookController.js
+++ app/Http/Controllers/WebhookController.js
@@ -1,10 +1,12 @@
 
 
 export default async function WebhookHelloController(request, response) {
-
+    const { event } = request.body;
     const HTTP_STATUS = CONSTANTS.HTTP;
 
-    console.log("Received webhook request:", request.body);
+    if (event === "payment.approved") {
+        console.log("payment.approved", request.body);
+    }
 
     return response.status(HTTP_STATUS.SUCCESS).json({
         "message": "Received webhook request successfully!"
